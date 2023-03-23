378aff1ca9c272c7382e6664e343b6674d2a9217
usamaahmadit8 <89377136+usamaahmadit8@users.noreply.github.com>
2023-03-23 13:37:57 +0500

    Changes in Whatsapp Message

--- app/Http/Controllers/WhatsappController.php
+++ app/Http/Controllers/WhatsappController.php
@@ -16,36 +16,48 @@ class WhatsappController extends Controller
     public function ChildTest(){
         $message='';
         $current_date=Carbon::now('Asia/Karachi')->format('Y-m-d');
-        $total_schools_lhr=ChildRosterUpdated::where('lab','=','LHR')->groupBy('school_code')->count();
-        $total_schools_Bwp=ChildRosterUpdated::where('lab','=','BWP')->groupBy('school_code')->count();
+        $current_date="2023-03-22";
+        $total_schools_lhr=ChildRosterUpdated::distinct('school_code')->where('lab','=','LHR')->count();
+        $total_schools_Bwp=ChildRosterUpdated::distinct('school_code')->where('lab','=','BWP')->count();
         $total_schools=$total_schools_Bwp+$total_schools_lhr;
-        $today_schools_lhr=ChildRosterUpdated::where('lab','=','LHR')->where(DB::raw("str_to_date(entry_start_time, '%d/%m/%Y')"),"=","$current_date")->groupBy('school_code')->count();
-        $today_schools_Bwp=ChildRosterUpdated::where('lab','=','BWP')->where(DB::raw("str_to_date(entry_start_time, '%d/%m/%Y')"),"=","$current_date")->groupBy('school_code')->count();
+        $today_schools_lhr=ChildRosterUpdated::distinct('school_code')->where('lab','=','LHR')->where(function($query) use ($current_date) {
+                $query->where(DB::raw("str_to_date(entry_start_time, '%d-%M-%y')"),"=","$current_date")->orWhere(DB::raw("str_to_date(entry_start_time, '%m/%d/%Y')"),"=","$current_date");
+            })->count();
+        $today_schools_Bwp=ChildRosterUpdated::distinct('school_code')->where('lab','=','BWP')->where(function($query) use ($current_date) {
+                $query->where(DB::raw("str_to_date(entry_start_time, '%d-%M-%y')"),"=","$current_date")->orWhere(DB::raw("str_to_date(entry_start_time, '%m/%d/%Y')"),"=","$current_date");
+            })->count();
         $today_schools=$today_schools_lhr+$today_schools_Bwp;
-        $total_schools_lhr_score=EnglishTest::where('lab','=','LHR')->groupBy('school_code')->count();
-        $total_schools_Bwp_score=EnglishTest::where('lab','=','BWP')->groupBy('school_code')->count();
+        $total_schools_lhr_score=EnglishTest::distinct('school_code')->where('lab','=','LHR')->count();
+        $total_schools_Bwp_score=EnglishTest::distinct('school_code')->where('lab','=','BWP')->count();
         $total_schools_score=$total_schools_Bwp_score+$total_schools_lhr_score;
-        $total_childs_lhr_score=EnglishTest::where('lab','=','LHR')->groupBy('school_code','class_code','section','child_id')->count();
-        $total_childs_Bwp_score=EnglishTest::where('lab','=','BWP')->groupBy('school_code','class_code','section','child_id')->count();
+        $total_childs_lhr_score=EnglishTest::distinct('school_code','class_code','section','child_id')->where('lab','=','LHR')->count();
+        $total_childs_Bwp_score=EnglishTest::distinct('school_code','class_code','section','child_id')->where('lab','=','BWP')->count();
         $total_childs_score=$total_childs_lhr_score+$total_childs_Bwp_score;
-        $today_childs_lhr_score=EnglishTest::where('lab','=','LHR')->where(DB::raw("str_to_date(entry_start_time, '%d/%m/%Y')"),"=","$current_date")->groupBy('school_code','class_code','section','child_id')->count();
-        $today_childs_Bwp_score=EnglishTest::where('lab','=','BWP')->where(DB::raw("str_to_date(entry_start_time, '%d/%m/%Y')"),"=","$current_date")->groupBy('school_code','class_code','section','child_id')->count();
+        $today_childs_lhr_score=EnglishTest::distinct('school_code','class_code','section','child_id')->where('lab','=','LHR')->where(function($query) use ($current_date) {
+                $query->where(DB::raw("str_to_date(entry_start_time, '%d-%M-%y')"),"=","$current_date")->orWhere(DB::raw("str_to_date(entry_start_time, '%m/%d/%Y')"),"=","$current_date");
+            })->count();
+        $today_childs_Bwp_score=EnglishTest::distinct('school_code','class_code','section','child_id')->where('lab','=','BWP')->where(function($query) use ($current_date) {
+                $query->where(DB::raw("str_to_date(entry_start_time, '%d-%M-%y')"),"=","$current_date")->orWhere(DB::raw("str_to_date(entry_start_time, '%m/%d/%Y')"),"=","$current_date");
+            })->count();
         $today_childs_score=$today_childs_lhr_score+$today_childs_Bwp_score;
-        
+        //echo $total_childs_lhr_score;
         $total_schools_lhr_grader=TipTest::where('office','=','LHR')->where('Received_Status','=','1')->count();
         $total_schools_Bwp_grader=TipTest::where('office','=','BWP')->where('Received_Status','=','1')->count();
         $total_schools_grader=$total_schools_Bwp_grader+$total_schools_lhr_grader;
         $totalSum_LHR = TipTest::where('office','=','LHR')->where('Received_Status','=','1')->sum('TotalCount');
         $totalSum_BWP = TipTest::where('office','=','BWP')->where('Received_Status','=','1')->sum('TotalCount');
         $total_childs_grader=$totalSum_BWP+$totalSum_LHR;
-        $todaySum_LHR = TipTest::where('office','=','LHR')->where(DB::raw("str_to_date(Date_of_Received, '%d/%m/%Y')"),"=","$current_date")->where('Received_Status','=','1')->sum('TotalCount');
-        $todaySum_BWP = TipTest::where('office','=','BWP')->where(DB::raw("str_to_date(Date_of_Received, '%d/%m/%Y')"),"=","$current_date")->where('Received_Status','=','1')->sum('TotalCount');
+        $todaySum_LHR = TipTest::where('office','=','LHR')->where(DB::raw("date(Date_of_Received)"),"=","$current_date")->where('Received_Status','=','1')->sum('TotalCount');
+        $todaySum_BWP = TipTest::where('office','=','BWP')->where(DB::raw("date(Date_of_Received)"),"=","$current_date")->where('Received_Status','=','1')->sum('TotalCount');
         $today_childs_grader=$todaySum_BWP+$todaySum_LHR;
         $message="--- RCONS --- Date = ".$current_date."\n\nApplication name = Child Test\n\n*ROSTER*\nTotal Schools = *$total_schools = ".$total_schools_lhr."L + ".$total_schools_Bwp."B*\nToday Schools = *$today_schools = ".$today_schools_lhr."L + ".$today_schools_Bwp."B*\n\n*SCORE SHEET*\nTotal Schools = *$total_schools_score = ".$total_schools_lhr_score."L + ".$total_schools_Bwp_score."B*\nTotal Childs = *$total_childs_score = ".$total_childs_lhr_score."L + ".$total_childs_Bwp_score."B*\nToday Childs = *$today_childs_score = ".$today_childs_lhr_score."L + ".$today_childs_Bwp_score."B*\n\n*GRADERS*\nTotal Schools = *".$total_schools_grader." = ".$total_schools_lhr_grader."L + ".$total_schools_Bwp_grader."B*\nTotal Childs = *".$total_childs_grader." = ".$totalSum_LHR."L + ".$totalSum_BWP."B*\nToday Childs = *".$today_childs_grader." = ".$todaySum_LHR."L + ".$todaySum_BWP."B*";
-       
-        $result= $this->whatsappNotification("+923008422788",$message);
-        $result= $this->whatsappNotification("+923008167076",$message);
-        $result= $this->whatsappNotification("+923217642430",$message);
+         $result= $this->whatsappNotification("+923008422788",$message);
+         $result= $this->whatsappNotification("+923008167076",$message);
+         $result= $this->whatsappNotification("+923217642430",$message);
+          $result= $this->whatsappNotification("+923038391109",$message);
+//           $mytime = Carbon::now();
+//   echo $mytime->toDateTimeString();
+ 
     }
     private function whatsappNotification(string $recipient,string $message)
     {
